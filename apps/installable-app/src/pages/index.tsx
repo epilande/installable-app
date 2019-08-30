7e2d896e9a7629054d78b1cae3a9141514354373
Emmanuel Pilande <epilande@gmail.com>
2019-08-29 22:24:34 -0700

    :sparkles: Display version in UI

--- apps/installable-app/src/pages/index.tsx
+++ apps/installable-app/src/pages/index.tsx
@@ -6,8 +6,9 @@ import Layout from "../components/Layout";
 const HomePage = () => {
   return (
     <Layout>
-      <h1>
-        <pre>Hello, {slugify("Installable App Demo")}</pre>
+      <h1 style={{ display: "flex", justifyContent: "space-between" }}>
+        <code>Hello, {slugify("Installable App Demo")}</code>
+        <code>v{process.env.APP_VERSION}</code>
       </h1>
       <h3>Math utils:</h3>
       <p>
